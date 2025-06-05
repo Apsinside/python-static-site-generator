fbff226d0b744a27d3618d35d7a052ab05e518ac
Apsinisde <fzickwolff@googlemail.com>
2025-06-05 15:46:28 +0200

    CH3: Inline | L4: Extract Links

--- src/main.py
+++ src/main.py
@@ -1,12 +1,11 @@
 from textnode import TextNode,TextType
-from inline_markdown import split_nodes_delimiter
+from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
+from extract_markdown import 
 def main():
-    text = "This is text with a **bolded word** and **another**"
-    print(text.split("**"))
-    node = TextNode(
-        "This is text with a **bolded word** and **another**", TextType.TEXT
-    )
-    new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
-    print (new_nodes)
-
+    img_text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+    link_text= "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotde)"
+    print(extract_markdown_images(img_text))
+    print(extract_markdown_images(link_text))
+    print(extract_markdown_links(img_text))
+    print(extract_markdown_links(link_text))
 main()
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,8 @@
 import unittest
 from inline_markdown import (
     split_nodes_delimiter,
+    extract_markdown_links,
+    extract_markdown_images
 )
 
 from textnode import TextNode, TextType 
@@ -84,3 +86,16 @@ class TestInlineMarkdown(unittest.TestCase):
             ],
             new_nodes,
         )
+
+    def test_extract_markdown_images(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        matches = extract_markdown_images(text)
+        self.assertListEqual(matches, [('rick roll', 'https://i.imgur.com/aKaOqIh.gif'), ('obi wan', 'https://i.imgur.com/fJRm4Vk.jpeg')])
+
+    def test_extract_markdown_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        matches = extract_markdown_links(text)
+        self.assertListEqual(matches, [('to boot dev', 'https://www.boot.dev'), ('to youtube', 'https://www.youtube.com/@bootdotdev')])
+
+if __name__ == "__main__":
+    unittest.main()
